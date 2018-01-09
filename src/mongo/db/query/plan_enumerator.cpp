4f1ba419eb3613178c85c078c1d45fa96142e0eb
Martin Neupauer <martin.neupauer@mongodb.com>
2018-01-09 16:30:03 -0500

    lint

--- src/mongo/db/query/plan_enumerator.cpp
+++ src/mongo/db/query/plan_enumerator.cpp
@@ -244,7 +244,8 @@ void tagForSort(MatchExpression* tree) {
             }
         }
         if (nodeWithSmallestTag) {
-            tree->setTag(new IndexTag(*static_cast<const IndexTag*>(nodeWithSmallestTag->getTag())));
+            tree->setTag(
+                    new IndexTag(*static_cast<const IndexTag*>(nodeWithSmallestTag->getTag())));
         }
     }
 }
