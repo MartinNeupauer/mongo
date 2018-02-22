47eae81289da97836f6257569c913a7fb5edf544
David Storch <david.storch@10gen.com>
2018-02-22 15:05:36 -0500

    Make project a cabal package.

---
+++ src/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import qualified Mongo.Bool3VL as Bool3VL
+import qualified Mongo.EvalExpr as EvalExpr
+import qualified Mongo.Expression as Expression
+import qualified Mongo.Util as Util
+import qualified Mongo.Variant as Variant
+
+main :: IO ()
+main = return ()
