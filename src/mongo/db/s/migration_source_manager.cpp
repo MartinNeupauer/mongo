f3212717dac0218ff6c40bf454789eec4180cd99
Martin Neupauer <martin.neupauer@10gen.com>
2018-03-07 11:46:41 -0500

    SERVER-33454 Remove the 'chunks map' from CollectionMetadata
    Removed _chunksMap from the CollectionMetadata class so the class is a
    simple passthrough wrapper over the ChunkManager.
    As a result getChunks() is a bit less efficient now as it recomputes the
    range map on every call but it is used rarely.

--- src/mongo/db/s/migration_source_manager.cpp
+++ src/mongo/db/s/migration_source_manager.cpp
@@ -391,9 +391,8 @@ Status MigrationSourceManager::commitChunkMetadataOnConfig(OperationContext* opC
             return status;
 
         boost::optional<ChunkType> controlChunkType = boost::none;
-        if (metadata->getNumChunks() > 1) {
-            ChunkType differentChunk;
-            invariant(metadata->getDifferentChunk(_args.getMinKey(), &differentChunk));
+        ChunkType differentChunk;
+        if (metadata->getDifferentChunk(_args.getMinKey(), &differentChunk)) {
             invariant(differentChunk.getMin().woCompare(_args.getMinKey()) != 0);
             controlChunkType = std::move(differentChunk);
         } else {
