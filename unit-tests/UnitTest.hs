443f345c7223fcecfc29eb1e299f5bd61f65610b
David Storch <david.storch@10gen.com>
2018-03-28 11:00:45 -0400

    Implement UnitTest.hs main function.

--- unit-tests/UnitTest.hs
+++ unit-tests/UnitTest.hs
@@ -1,3 +1,17 @@
-main :: IO ()
-main =
-    return ()
+module Main where
+
+import System.Exit (exitFailure, exitSuccess)
+import Test.HUnit
+
+-- Import suites of unit tests from test modules, and add them to this list.
+allTests :: Test
+allTests = TestList []
+
+main :: IO Counts
+main = do
+    results <- runTestTT allTests
+    if errors results + failures results > 0
+    then
+        exitFailure
+    else
+        exitSuccess
